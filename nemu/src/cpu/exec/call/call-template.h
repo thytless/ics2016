c86636e9c66df6c4ee923446cbcfe7f8329a0e0f
tracer-ics2016 <tracer@njuics.org>
2016-10-03 23:19:43 -0700

    >  compile NEMU 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  23:19:43 up  7:25,  2 users,  load average: 0.01, 0.03, 0.05 e170fb098abee902ee821ee250deeadaac36a357

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -21,7 +21,12 @@ static void do_execute () {
 }
 
 #if DATA_BYTE == 2 || DATA_BYTE == 4
-make_instr_helper(i)
+make_helper(concat(call_i_,SUFFIX)){
+	int len = concat(decode_i_,SUFFIX)(cpu.eip + 1);
+	cpu.eip += len + 1;
+	do_execute();
+	return 0;
+}
 #endif
 
 #include "cpu/exec/template-end.h"
