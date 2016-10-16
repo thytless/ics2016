49b08c47504d015b3429617f8f6ed3bc07b990ff
tracer-ics2016 <tracer@njuics.org>
2016-10-16 08:31:47 -0700

    >  compile NEMU 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  08:31:47 up 21:10,  3 users,  load average: 0.07, 0.05, 0.05 abe8b3e3554afa55649ec65e522edcc8fe012cbf

--- nemu/src/cpu/exec/jump/jle-template.h
+++ nemu/src/cpu/exec/jump/jle-template.h
@@ -4,7 +4,7 @@
 
 static void do_execute(){
 	int32_t disp = op_src->val;
-	assert(op_src->val == 0xa3);
+	assert(disp == 0xa3);
 	if(cpu.eflags._of != cpu.eflags._sf || cpu.eflags._zf)
 		cpu.eip += disp;
 	print_asm_template1();
