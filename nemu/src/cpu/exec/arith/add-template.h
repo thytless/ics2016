50346da5493409873637fbbd1a5a76914c9ecc72
tracer-ics2016 <tracer@njuics.org>
2016-10-16 09:48:31 -0700

    >  compile NEMU 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  09:48:31 up 22:27,  3 users,  load average: 0.00, 0.01, 0.05 dd12c1b03dcedac616ff6488863e6958140d8f8b

---
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -0,0 +1,44 @@
+#include<cpu/exec/template-start.h>
+
+#define instr add
+
+static void do_execute(){
+	DATA_TYPE_S temp = (op_src->size == 8) ? (int8_t)op_src->val : op_src->val;
+	DATA_TYPE_S sum = op_dest->val + temp;
+	OPERAND_W(op_dest,sum);
+
+	cpu.eflags._zf = sum ? 0 : 1;
+	cpu.eflags._sf = (sum < 0) ? 1 : 0;
+
+	if(op_src->val > 0 && op_dest->val > 0 && sum < op_src->val)
+		cpu.eflags._cf = 1;
+	else if(op_src->val < 0 && op_dest->val < 0 && sum > op_src->val)
+		cpu.eflags._cf = 1;
+	else
+		cpu.eflags._cf = 0;
+
+	if(op_src->val > 0 && op_dest->val > 0 && sum < 0)
+		cpu.eflags._of = 1;
+	else if(op_src->val < 0 && op_dest->val < 0 && sum > 0)
+		cpu.eflags._of = 1;
+	else
+		cpu.eflags._of = 0;
+	int n = 0,i;
+	for(i = 0;i < 8;i++,sum = sum >> 1)
+		if(sum & 0x1)
+			n++;
+	cpu.eflags._pf = (n % 2) ? 1 : 0;
+	print_asm_template2();
+}
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm);
+#endif
+make_instr_helper(i2a);
+make_instr_helper(i2rm);
+make_instr_helper(r2rm);
+make_instr_helper(rm2r);
+
+#include<cpu/exec/template-end.h>
+
+
