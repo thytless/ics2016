9640ecbb22be50f6a7467a55be76b318bd3fceb6
tracer-ics2016 <tracer@njuics.org>
2016-10-15 22:31:28 -0700

    >  compile NEMU 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  22:31:28 up 13:09,  3 users,  load average: 0.00, 0.01, 0.05 7c791cb2de018b67bf039c0134f4e08a50791c11

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -1,12 +1,27 @@
 #include<cpu/exec/template-start.h>
 
 #define instr sub
-
+/*IF	SRC is a byte and DEST is a word or dword
+ *THEN	DEST -= signextend(SRC);
+ *ELSE	DEST -= SRC;
+ *FI
+*/
 static void do_execute(){
-
+	if(op_src->type == 1){
+		int32_t temp = op_src->val;
+		temp = op_dest->val - temp;
+		OPERAND_W(op_dest,temp);
+	}
+	else{
+		uint32_t utemp = op_src->val;
+		utemp = op_dest->val - utemp;
+		OPERAND_W(op_dest,utemp);
+	}
+	
+	
 }
 
-
+make_instr_helper(si2rm);
 
 
 
