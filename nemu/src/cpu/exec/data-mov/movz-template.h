468d96e020e80d492ecf126e22670c1f0aba4d15
tracer-ics2016 <tracer@njuics.org>
2016-10-17 05:43:53 -0700

    >  compile NEMU 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  05:43:53 up 1 day, 18:22,  3 users,  load average: 0.09, 0.03, 0.05 475f880365e3a318d17d10881f83b954dc8b606e

---
+++ nemu/src/cpu/exec/data-mov/movz-template.h
@@ -0,0 +1,23 @@
+#include<cpu/exec/template-start.h>
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+#define instr movzb
+static void do_execute(){
+	OPERAND_W(op_dest,(DATA_TYPE)op_src->val);
+	print_asm_template2();
+}
+make_instr_helper(rm2r);
+#undef instr
+#endif
+
+#if DATA_BYTE == 4
+#define instr movzw
+static void do_execute(){
+	OPERAND_W(op_dest,(DATA_TYPE)op_src->val);
+	print_asm_template2();
+}
+make_instr_helper(rm2r);
+#endif
+#include<cpu/exec/template-end.h>
+
+
