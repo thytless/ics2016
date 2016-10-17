df514c4e729d8b7089c5aa4d02dc08fb84cd9a9b
tracer-ics2016 <tracer@njuics.org>
2016-10-17 05:23:23 -0700

    >  compile NEMU 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  05:23:23 up 1 day, 18:01,  3 users,  load average: 0.06, 0.05, 0.05 3d0b4e4b3337889f8a9a30a9e91e2ff99a446c0c

--- nemu/src/cpu/exec/logic/or-template.h
+++ nemu/src/cpu/exec/logic/or-template.h
@@ -6,8 +6,15 @@ static void do_execute () {
 	DATA_TYPE result = op_dest->val | op_src->val;
 	OPERAND_W(op_dest, result);
 
-	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	cpu.eflags._cf = 0;
+	cpu.eflags._of = 0;
+	cpu.eflags._zf = result ? 0 : 1;
+	cpu.eflags._sf = result >> (DATA_BYTE * 8 - 1);
+	int n = 0,i;
+	for(i = 0;i < 8;i++,result = result >> 1)
+		if(result & 0x1)
+			n++;
+	cpu.eflags._pf = (n % 2) ? 1 : 0;
 
 	print_asm_template2();
 }
