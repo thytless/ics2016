020b00dceb372f1e4f42b9e4e6d8400acd7d4f39
tracer-ics2016 <tracer@njuics.org>
2016-10-17 02:12:31 -0700

    >  compile NEMU 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  02:12:30 up 1 day, 14:51,  3 users,  load average: 0.00, 0.01, 0.05 bb0a852d4d732c844f4ace5e77532bf8bd364584

--- nemu/src/cpu/exec/call/leave-template.h
+++ nemu/src/cpu/exec/call/leave-template.h
@@ -7,12 +7,13 @@ make_helper(leave){
 	op_dest->type = OP_TYPE_REG;
 	op_dest->reg = R_ESP;
 	op_dest->val = REG(R_ESP);
-	OPERAND_W(op_dest,REG(R_EBP));
 
 	op_src->type = OP_TYPE_REG;
 	op_src->reg = R_EBP;
 	op_src->val = REG(R_EBP);
 
+	OPERAND_W(op_dest,op_src->val);
+	
 	uint32_t temp = swaddr_read(op_dest->val,4);
 	OPERAND_W(op_src,temp);
 	temp = op_dest->val + 4;
