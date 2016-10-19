21b076a24f827aad830d3f74991ab464e392cb38
tracer-ics2016 <tracer@njuics.org>
2016-10-18 22:37:30 -0700

    >  compile NEMU 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  22:37:30 up 2 days, 10:48,  3 users,  load average: 0.01, 0.02, 0.05 9fb0f31b742e51822dee5548aebb995344658cbf

--- nemu/src/cpu/exec/jump/jmp-template.h
+++ nemu/src/cpu/exec/jump/jmp-template.h
@@ -3,7 +3,7 @@
 #define instr jmp
 
 make_helper(concat(jmp_i_,SUFFIX)){
-	int len = concat(decode_i_,SUFFIX)(eip);
+	int len = concat(decode_i_,SUFFIX)(eip + 1);
 	int32_t disp = (DATA_TYPE_S)op_src->val;
 	cpu.eip += disp;
 	print_asm("jmp" str(SUFFIX) " %x",op_src->val);
@@ -12,7 +12,7 @@ make_helper(concat(jmp_i_,SUFFIX)){
 
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 make_helper(concat(jmp_rm_,SUFFIX)){
-	int len = concat(decode_rm_,SUFFIX)(eip);
+	int len = concat(decode_rm_,SUFFIX)(eip + 1);
 	cpu.eip = (DATA_BYTE == 2) ? 0x0000FFFF & op_src->val : op_src->val;
 	print_asm("jmp" str(SUFFIX) " %d",cpu.eip);
 	return len + 1;
