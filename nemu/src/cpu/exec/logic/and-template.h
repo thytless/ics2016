fe81130bc96eb2535a7bda0d327ffa212f3b96df
tracer-ics2016 <tracer@njuics.org>
2016-10-18 17:52:13 -0700

    >  compile NEMU 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  17:52:13 up 2 days,  7:28,  4 users,  load average: 0.06, 0.04, 0.05 a176e3304b8cf4e7d01d3d3e56f56512a6a6c571

--- nemu/src/cpu/exec/logic/and-template.h
+++ nemu/src/cpu/exec/logic/and-template.h
@@ -3,7 +3,7 @@
 #define instr and
 
 static void do_execute () {
-	DATA_TYPE_S temp = (DATA_TYPE_S)op_src->val;
+	DATA_TYPE_S temp = (DATA_TYPE_S)op_src->simm;
 	assert(temp == 0x000000f0);
 	DATA_TYPE result = temp & op_dest->val;
 	OPERAND_W(op_dest, result);
