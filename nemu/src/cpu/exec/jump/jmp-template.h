126b62a6aaf9dbd58b07c2fdb66694db4288e1c2
tracer-ics2016 <tracer@njuics.org>
2016-10-18 22:56:46 -0700

    >  compile NEMU 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  22:56:46 up 2 days, 11:08,  3 users,  load average: 0.00, 0.01, 0.05 b69b6d996da08d7451150ff62ca261aaa9054d38

--- nemu/src/cpu/exec/jump/jmp-template.h
+++ nemu/src/cpu/exec/jump/jmp-template.h
@@ -2,21 +2,20 @@
 
 #define instr jmp
 
-make_helper(concat(jmp_i_,SUFFIX)){
-	int len = concat(decode_i_,SUFFIX)(eip + 1);
-	int32_t disp = (DATA_TYPE_S)op_src->val;
-	cpu.eip += disp;
-	print_asm("jmp" str(SUFFIX) " %x",op_src->val);
-	return len + 1;
+static void do_execute(){
+	int32_t temp = (DATA_TYPE_S)op_src->val;
+	switch(ops_decoded.opcode){
+		case 0xeb:case 0xe9:
+			cpu.eip += temp;break;
+		case 0xFF:
+			cpu.eip = temp;
+	}
+	print_asm("jmp" str(SUFFIX) " %x",cpu.eip);
 }
 
+make_instr_helper(i);
 #if DATA_BYTE == 2 || DATA_BYTE == 4
-make_helper(concat(jmp_rm_,SUFFIX)){
-	int len = concat(decode_rm_,SUFFIX)(eip);
-	cpu.eip = (DATA_BYTE == 2) ? 0x0000FFFF & op_src->val : op_src->val;
-	print_asm("jmp" str(SUFFIX) " %d",cpu.eip);
-	return len + 1;
-}
+make_instr_helper(rm);
 #endif
 
 #include<cpu/exec/template-end.h>
