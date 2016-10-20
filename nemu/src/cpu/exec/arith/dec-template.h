5ec13caf4690c26e3b6ba411d75f5ba0a26cddbb
tracer-ics2016 <tracer@njuics.org>
2016-10-20 06:49:50 -0700

    >  compile NEMU 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  06:49:50 up 2 days, 16:55,  3 users,  load average: 0.09, 0.04, 0.05 a7c9eac5eb3c8379aab9797fafe4857bc1455a7

--- nemu/src/cpu/exec/arith/dec-template.h
+++ nemu/src/cpu/exec/arith/dec-template.h
@@ -3,11 +3,23 @@
 #define instr dec
 
 static void do_execute () {
-	DATA_TYPE result = op_src->val - 1;
-	OPERAND_W(op_src, result);
+	DATA_TYPE sub = op_src->val - 1;
+	OPERAND_W(op_src, sub);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	cpu.eflags._zf = sub ? 0 : 1;
+	cpu.eflags._sf = sub >> (DATA_BYTE * 8 - 1);
+	cpu.eflags._cf = ((DATA_TYPE_S)op_src->val < 1);
+	bool src_sign = op_src->val >> (op_src->size * 8 - 1);
+	if(!src_sign && !cpu.eflags._sf)
+		cpu.eflags._of = 1;
+	else
+		cpu.eflags._of = 0;
+	int n = 0,i;
+	for(i = 0;i < 8;i++,sub = sub >> 1)
+		if(sub & 0x1)
+			n++;
+	cpu.eflags._pf = (n % 2) ? 1 : 0;
 
 	print_asm_template1();
 }
--- nemu/src/cpu/exec/arith/inc-template.h
+++ nemu/src/cpu/exec/arith/inc-template.h
@@ -3,11 +3,29 @@
 #define instr inc
 
 static void do_execute () {
-	DATA_TYPE result = op_src->val + 1;
-	OPERAND_W(op_src, result);
+	DATA_TYPE sum = op_src->val + 1;
+	OPERAND_W(op_src, sum);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+
+	cpu.eflags._zf = sum ? 0 : 1;
+	cpu.eflags._sf = sum >> (DATA_BYTE * 8 - 1);
+
+	if(op_src->val > 0 && sum < op_src->val)
+		cpu.eflags._cf = 1;
+	else
+		cpu.eflags._cf = 0;
+
+	bool src_sign = op_src->val >> (op_src->size * 8 - 1);
+	if(!src_sign && cpu.eflags._sf)
+		cpu.eflags._of = 1;
+	else
+		cpu.eflags._of = 0;
+	int n = 0,i;
+	for(i = 0;i < 8;i++,sum = sum >> 1)
+		if(sum & 0x1)
+			n++;
+	cpu.eflags._pf = (n % 2) ? 1 : 0;
 
 	print_asm_template1();
 }
