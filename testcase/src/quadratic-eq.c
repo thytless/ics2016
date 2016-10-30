046a6dd68dcdf95167e3a4fc6f56b789b1ded475
tracer-ics2016 <tracer@njuics.org>
2016-10-30 06:12:20 -0700

    >  run 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  06:12:20 up  3:23,  3 users,  load average: 0.00, 0.03, 0.05 586910b828cbb2181c5aed8e70c726f3dcce3a5d

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -4,19 +4,26 @@
 /* solve 0.5x^2 + 0.5x - 0.5 = 0 */
 
 int main() {
+//	FLOAT a = f2F(0.5);
+//	FLOAT b = f2F(0.5);
+//	FLOAT c = f2F(-0.5);
+
 	FLOAT a = f2F(0.5);
 	FLOAT b = f2F(0.5);
-	FLOAT c = f2F(-0.5);
+	FLOAT c = f2F(-1);
 
 	FLOAT dt = F_mul_F(b, b) - F_mul_F(F_mul_int(a, 4), c);
 	FLOAT sqrt_dt = sqrt(dt);
-	nemu_assert(dt == f2F(-0.75));
+	nemu_assert(sqrt_dt == f2F(1.5));
 
 	FLOAT x1 = F_div_F(-b + sqrt_dt, F_mul_int(a, 2));
 	FLOAT x2 = F_div_F(-b - sqrt_dt, F_mul_int(a, 2));
 
-	FLOAT x1_ans = f2F(0.618);
-	FLOAT x2_ans = f2F(-1.618);
+//	FLOAT x1_ans = f2F(0.618);
+//	FLOAT x2_ans = f2F(-1.618);
+
+	FLOAT x1_ans = f2F(1);
+	FLOAT x2_ans = f2F(-2);
 
 	nemu_assert(Fabs(x1_ans - x1) < f2F(1e-4));
 	nemu_assert(Fabs(x2_ans - x2) < f2F(1e-4));
