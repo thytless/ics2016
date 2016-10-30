3181bb5c606588a93a29bb92c4fe77827bafd85e
tracer-ics2016 <tracer@njuics.org>
2016-10-30 06:42:51 -0700

    >  run 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  06:42:51 up  3:54,  3 users,  load average: 0.00, 0.02, 0.05 99cdf0b88efe386d1fcf0a8ba7229093a47971c0

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -9,7 +9,6 @@ int main() {
 	FLOAT c = f2F(-0.5);
 
 	FLOAT dt = F_mul_F(b, b) - F_mul_F(F_mul_int(a, 4), c);
-	nemu_assert(dt == f2F(1.25));
 	FLOAT sqrt_dt = sqrt(dt);
 
 	FLOAT x1 = F_div_F(-b + sqrt_dt, F_mul_int(a, 2));
