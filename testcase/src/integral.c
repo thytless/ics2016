a714ab3dcee0475aba9d0deb38b449f8540c1610
tracer-ics2016 <tracer@njuics.org>
2016-10-30 08:25:45 -0700

    >  run 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  08:25:45 up  5:37,  3 users,  load average: 0.15, 0.07, 0.06 9fa4fccc2d74da1bb29194ef69185961661adfdf

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -12,7 +12,7 @@ FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
 	h = F_div_int((b - a), n);
 	s = F_div_int(fun(a) + fun(b), 2 );
 //	nemu_assert(Fabs( f2F(0.0625) - f2F(1/16) ) < f2F(1e-4));
-	nemu_assert(f2F(1/16) == f2F(0.0625));
+	nemu_assert(1/16 == 0.0625);
 
 	for(k = 1; k < n; k ++) {
 		s += fun(a + F_mul_int(h, k));
