36ea1814664df289508e1aad7cdb0d8028406edb
tracer-ics2016 <tracer@njuics.org>
2016-10-30 04:14:52 -0700

    >  compile NEMU 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  04:14:52 up  1:30,  2 users,  load average: 0.02, 0.04, 0.05 d49e66cd3524841c9a6c2abaed73523c38592899

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -7,14 +7,15 @@ static void do_execute () {
 	//that is
 	//esp -= 4
 	//*esp = eip
-	op_dest->type = OP_TYPE_REG;
-	op_dest->reg = R_ESP;
-	op_dest->val = REG(R_ESP);
-	snprintf(op_dest->str, OP_STR_SIZE, "%s",REG_NAME(R_ESP));
+//	op_dest->type = OP_TYPE_REG;
+//	op_dest->reg = R_ESP;
+//	op_dest->val = REG(R_ESP);
+//	snprintf(op_dest->str, OP_STR_SIZE, "%s",REG_NAME(R_ESP));
 	
-	uint32_t temp = op_dest->val - 4;
+//	uint32_t temp = op_dest->val - 4;
+	uint32_t temp = cpu.esp - 4;
 	swaddr_write(temp,4,cpu.eip);
-	OPERAND_W(op_dest,temp);
+	cpu.esp = temp;
 	
 	int32_t disp = op_src->val;
 	cpu.eip += disp;
@@ -30,5 +31,13 @@ make_helper(concat(call_i_,SUFFIX)){
 	return len + 1;
 }
 #endif
-
+#if DATA_BYTE == 4
+make_helper(call_r_l){
+	int len = decode_r_l(eip + 1);
+	cpu.eip += len + 1;
+	do_execute();
+	cpu.eip -= len + 1;
+	return len + 1;
+}
+#endif
 #include "cpu/exec/template-end.h"
