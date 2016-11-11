2da154538202a91b7001e62e46810581fbeb6b4e
tracer-ics2016 <tracer@njuics.org>
2016-11-11 06:57:43 -0800

    >  compile NEMU 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  06:57:43 up 1 day, 16:14,  3 users,  load average: 0.01, 0.02, 0.05 4670b25b40a6e45a41e16bb43f50c6be569e25e6

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -14,12 +14,29 @@ uint32_t cache_read(swaddr_t addr, size_t size, bool *success){
 		CacheSlot *cs = &cache[temptag & SET_MASK][i];
 		if(cs->valid && !cs->dirty && temptag == cs->tag){
 			uint32_t disp = addr & CACHE_LINE_MASK;
-			*success = true;
-			switch(size){
-				case 1: return (uint32_t)cs->data[disp];
-				case 2: return (uint32_t)cs->data16[disp / 2];
-				case 4: return cs->data32[disp / 4];
-				default:panic("Bad Size!");
+			if(disp + size > CACHE_LINE_SIZE){
+				int lsize = CACHE_LINE_SIZE - disp - 1;
+				int rsize = size - lsize;
+				assert(rsize);
+				int ldata = 0;
+				int rdata = 0;
+				if(lsize)
+					ldata = cache_read(addr,lsize,success);
+				if(*success){
+					rdata = cache_read(addr + lsize,rsize,success);
+					if(*success)
+						return (ldata << (lsize * 8)) + rdata;
+				}
+				return 0;
+			}
+			else{
+				*success = true;
+				switch(size){
+					case 1: return cs->data[disp];
+					case 2: return cs->data16[disp / 2];
+					case 4: return cs->data32[disp / 4];
+					default:panic("Bad Size!");
+				}
 			}
 		}
 	}
@@ -35,15 +52,29 @@ void cache_write(swaddr_t addr, size_t size, uint32_t data){
 		CacheSlot *cs = &cache[temptag & SET_MASK][i];
 		if(cs->valid && !cs->dirty && temptag == cs->tag){
 			uint32_t disp = addr & CACHE_LINE_MASK;
-			switch(size){
-				case 1: cs->data[disp] = data & 0xff;break;
-				case 2: cs->data16[disp / 2] = data & 0xffff;break;
-				case 4: cs->data32[disp / 4] = data;break;
-				default:panic("Bad Size!");break;
+			if(disp + size > CACHE_LINE_SIZE){
+				int lsize = CACHE_LINE_SIZE - disp - 1;
+				int rsize = size - lsize;
+				int mask = (1 << rsize * 8) - 1;
+				assert(rsize);
+				if(lsize)
+					cache_write(addr,lsize,data >> (rsize * 8));
+				cache_write(addr+lsize,rsize,data & mask);
+				return;
+			}
+			else{
+				switch(size){
+					case 1: cs->data[disp] = data & 0xff;break;
+					case 2: cs->data16[disp / 2] = data & 0xffff;break;
+					case 4: cs->data32[disp / 4] = data;break;
+					default:panic("Bad Size!");break;
+				}
+				return;
 			}
 		}
 	}
 	load_cache(temptag);
+	return;
 }
 
 void load_cache(swaddr_t temptag){
