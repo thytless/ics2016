f0163353bcc842209858414875c4732e7bf7075d
tracer-ics2016 <tracer@njuics.org>
2016-12-04 06:42:04 -0800

    >  compile NEMU 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  06:42:04 up 1 day, 20:13,  4 users,  load average: 0.04, 0.04, 0.05 bfc63d8867ac9afeaa4781259a4fb855a4e78245

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -10,10 +10,9 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	bool success = true;
 	uint32_t cache_ret = cache_read(addr,len,&success);
 	uint32_t dram_ret = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
-	if(!success)Log("miss!");
 	if(success){
-		if(cache_ret != dram_ret)
-			Log("R:%x %x addr:%x len:%d",cache_ret,dram_ret,addr,len);
+//		if(cache_ret != dram_ret)
+//			Log("R:%x %x addr:%x len:%d",cache_ret,dram_ret,addr,len);
 		return cache_ret;
 	}
 	else
@@ -23,10 +22,11 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	dram_write(addr, len, data);
 	cache_write(addr, len, data);
-	bool success = true;
+/*	bool success = true;
 	int temp = cache_read(addr, len, &success);
 	if(temp != data)
 		Log("W:%x %x addr:%x len:%d",temp,data,addr,len);
+*/
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
