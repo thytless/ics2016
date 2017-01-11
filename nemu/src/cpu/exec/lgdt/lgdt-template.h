eb0b1ebfa23b25a9b6b06e5349a3388663ec3ed5
tracer-ics2016 <tracer@njuics.org>
2017-01-11 01:11:39 -0800

    >  compile NEMU 151220062 thyt Linux ubuntu 4.4.0-21-generic #37-Ubuntu SMP Mon Apr 18 18:34:49 UTC 2016 i686 i686 i686 GNU/Linux  01:11:39 up 3 days, 18:23,  4 users,  load average: 0.02, 0.03, 0.05 479d9ead8666f63a59de1a31c628d9e633dc465

--- nemu/src/cpu/exec/lgdt/lgdt-template.h
+++ nemu/src/cpu/exec/lgdt/lgdt-template.h
@@ -4,6 +4,7 @@
 
 make_helper(lgdt){
 	uint32_t addr = swaddr_read(eip + 1,4);
+	Log("0x%x",addr);
 	cpu.GDTR.gdtLimit = swaddr_read(addr,2);
 	cpu.GDTR.gdtBase = swaddr_read(addr + 2,4);
 	print_asm("lgdt 0x%x ", addr);
